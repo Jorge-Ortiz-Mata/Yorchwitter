a27b795e4417df2bb9adc175577376e784d5c999
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-02-21 14:20:14 -0600

    User show modified.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
     before_action :require_account
 
     def home
+        @users = User.order('created_at DESC')
         @posts = Post.order('created_at DESC')
     end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     
         if account && account.authenticate(params[:session][:password])   # <--- If the user logged_in succesfully.
             session[:user_id] = account.id                             # <-- Save the user into the session.
-            redirect_to root_path, notice: "Successfully loged in."
+            redirect_to root_path, notice: "Welcome: ¡ #{account.user.first_name} #{account.user.last_name} !"
         else
             redirect_to signin_path, alert: "Invalid params."
         end
